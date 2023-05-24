3ba895339bdc37853e8c71b2ab5a855ecbb2f2bf
Dushmanta <dushmanta0511@gmail.com>
2023-05-25 00:03:40 +0530

    add eventListener to mobile-nav element

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let hamberger = document.querySelector('.hamberger');
 let times = document.querySelector('.times');
 let mobileNav = document.querySelector('.mobile-nav')
+let navbarLinks = document.querySelectorAll('.mobile-nav a');
 
 hamberger.addEventListener('click', function(){
     mobileNav.classList.add('open')
@@ -9,3 +10,22 @@ times.addEventListener('click', function(){
     mobileNav.classList.remove('open')
 })
 
+navbarLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      mobileNav.classList.remove('open');
+  
+      // Get the target element from the link's href attribute
+      let targetId = link.getAttribute('href').substring(1);
+      let targetElement = document.getElementById(targetId);
+  
+      if (targetElement) {
+        setTimeout(function() {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }, 500);
+      }
+    });
+  });
